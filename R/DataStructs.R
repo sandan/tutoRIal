080a16a70676bb771189762c97198f0b9e912784
Mark Sandan <Mark.Sandan@Teradata.com>
2017-10-16 13:42:28 -0700

    define create_things

---
+++ R/DataStructs.R
@@ -0,0 +1,13 @@
+
+# Define basic building blocks for the package
+# The main building block is the 'thing'
+# A thing has other things...
+# This lends a thing to be internally stored as a list
+# (i.e mode(thing)) -> list
+
+create_thing<-function(things=NULL){
+
+  t<-list(things)
+  attr(t, "class")<-"thing"
+  return (t)
+}
